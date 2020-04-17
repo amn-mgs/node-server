0be23058e1ae198643cde3b0d8d5dd578288f981
Ali Mohammed <amn.ali_new@yahoo.com>
2020-04-17 21:46:53 +0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
-const mysql = require('mysql');
+// const mysql = require('mysql');
+const { Pool } = require('pg');
 const socketio = require('socket.io');
 const bodyParser = require('body-parser');
 const config = require('./constants');
@@ -14,40 +15,66 @@ let io = socketio(server);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(express.json);
 
+//configure the database Connection 
+var pool = new Pool({
+   connectionString: process.env.DATABASE_URL,
+  // connectionString: config.connectionString,
+  // user: config.DbUserName,
+  // password: config.DbPassword,
+  // host: config.DbHost,
+  // port: config.DbPort,
+  // database: config.Dbname,
+  ssl: config.ssl,
+});
+
+//start the server to be listening 
 server.listen(port, () => console.log(`server is started at port ${port}`));
+
 
 app.post('/user', (req, res, err) => {
   console.log(req.body);
   res.status(200).send(req.body);
 });
 
-app.get('/', (req, res) => {
-  console.log('in app get /');
-  const con = mysql.createConnection({ host: config.DbHost, database: config.Dbname, user: config.DbUserName, password: config.DbPassword });
-  con.connect();
-  // console.log(con);
-  // checkcontodb(con);
-  con.query('select * from users;', (err, rs) => {
-    if (err) throw err;
-    rs.forEach(element => {
-      console.log(element.id, element.userName);
-    });
-    res.status(200).send(rs).json;
-    console.log(rs);
-    con.end();
+app.get('/', async (req, res) => {
+  try {
+    const con = await pool.connect();
+    const result = await con.query('select * from users;');
+    console.log(result);
+    res.send(result).json;
+
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Amn Error" + err);
+  }
+});
+
+
+function checkcontodb(c) {
+  c.query('select 1+1 as sol', (err, res, fields) => {
+    if (err) throw error;
+    ;
+    console.log('the sol is = ', res[0].sol);
   });
-
-  
-   // res.status(200).send('hello world form node js server').json;
-});
-
-function checkcontodb(c){
-c.query('select 1+1 as sol', (err,res,fields)=>{
-  if(err)throw error;
-  ;
-  console.log('the sol is = ',res[0].sol);
-});
 }
 
+
+
+// //this used with mysql db but we will not need it right now
+// app.get('/mysql', (req, res) => {
+//   console.log('in app get /');
+//   const con = mysql.createConnection({ host: config.DbHost, database: config.Dbname, user: config.DbUserName, password: config.DbPassword });
+//   con.connect();
+//   // console.log(con);
+//   // checkcontodb(con);
+//   con.query('select * from users;', (err, rs) => {
+//     if (err) throw err;
+//     rs.forEach(element => {
+//       console.log(element.id, element.userName);
+//     });
+//     res.status(200).send(rs).json;
+//     console.log(rs);
+//     con.end();
+//   });
+// });
