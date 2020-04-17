080a7513f68613dc876f027032d6a18e15080f49
Ali Mohammed <amn.ali_new@yahoo.com>
2020-04-17 17:01:48 +0300

    test post

---
+++ constants.js
@@ -0,0 +1,16 @@
+
+
+// let DbHost='ec2-54-247-89-181.eu-west-1.compute.amazonaws.com';
+// let DbPort='5432';
+// let Dbname='d118jlpdg3tra9';
+// let DbUserName='ezyywaujxtlotu';
+// let DbPassword='b05f1caa7e79bd13c0906fde89d5ea75c39c392cfea99ec92f8b56ee5885fe6c';
+
+
+let DbHost = 'localhost';
+let DbPort = '3000';
+let Dbname = 'medical';
+let DbUserName = 'ali';
+let DbPassword = 'ali';
+
+module.exports = { DbHost, DbPort, Dbname, DbUserName, DbPassword };
--- index.js
+++ index.js
@@ -2,32 +2,40 @@ const http = require('http');
 const express = require('express');
 const mysql = require('mysql');
 const socketio = require('socket.io');
+const bodyParser = require('body-parser');
+const config = require('./constants');
+//to check if the port is equipped or not write in cmd
+//cmd: netstat -ano | find ":3001" 
 port = process.env.PORT || 3000;
 const app = express();
 
 let server = http.createServer(app);
 let io = socketio(server);
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.json);
+
 server.listen(port, () => console.log(`server is started at port ${port}`));
 
-
-
-app.get('/', (req, res) => {
-
-  // const con = mysql.createConnection({ host: 'localhost', database: 'medical', user: 'ali', password: 'ali' });
-  // con.connect();
-  res.status(200).send('hello world form node js server').json;
-  // con.query('select * from users', (err, rs) => {
-  //   if (err) throw err;
-  //   // rs.forEach(element => {
-  //   //   console.log(element.id, element.userName);
-  //   // });
-  //   res.status(200).send(rs).json;
-  //   console.log(rs);
-  //   con.end();
-  // });
+app.post('/user', (req, res, err) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
 });
 
-// app.listen(3000, () => {
-//   console.log('your server is started at port 3000');
-// })
+app.get('/', (req, res) => {
+  console.log('in app get /');
+  const con = mysql.createConnection({ host: config.DbHost, database: config.Dbname, user: config.DbUserName, password: config.DbPassword });
+  con.connect();
+  // res.status(200).send('hello world form node js server').json;
+  con.query('select * from users', (err, rs) => {
+    if (err) throw err;
+    rs.forEach(element => {
+      // console.log(element.id, element.userName);
+    });
+    res.status(200).send(rs).json;
+    // console.log(rs);
+    con.end();
+  });
+});
+
